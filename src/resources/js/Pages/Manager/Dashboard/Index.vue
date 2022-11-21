a922f663bb0c2f921623fc1e7876646153b42853
German Middi <german.middi@gmail.com>
2022-11-20 21:36:13 -0300

    Ajustes Dashboard

--- src/resources/js/Pages/Manager/Dashboard/Index.vue
+++ src/resources/js/Pages/Manager/Dashboard/Index.vue
@@ -10,6 +10,94 @@
                 </a>
             </div>
         </template>
+
+        <div class="py-2">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 card">
+                <ul class="top-filter">
+                    <li class="top-filter-item">Todos <span class="top-filter-number">50</span> </li>
+                    <li class="top-filter-item top-filter-item-active">Enviados <span class="top-filter-number">10</span></li>
+                    <li class="top-filter-item">Enviados(Retiro Pendiente) <span class="top-filter-number">10</span></li>
+                    <li class="top-filter-item">Retiros <span class="top-filter-number">10</span></li>
+                    <li class="top-filter-item">Finalizados <span class="top-filter-number">10</span></li>
+                </ul> 
+            </div>
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 card py-10 my-5">
+                <div class="card-header">
+                    <div class="card-title">Próximos Pedidos</div> 
+                    <div class="card-buttons">
+                        <button class="button-w-icon">Filtros <ChevronDownIcon class="w-5" /></button>
+                    </div>    
+                </div>
+                <div class="card-filter">
+                    <div class="filter-input-group">
+                        <label>Direccón: </label>
+                        <input />   
+                    </div>    
+                    <div class="filter-input-group">
+                        <!-- <label>Direccón: </label> -->
+                        <input placeholder="Cliente"/>   
+                    </div>    
+                    <div class="filter-input-group">
+                        <!-- <label>Direccón: </label> -->
+                        <input />   
+                    </div>    
+                    <div class="filter-input-group">
+                        <!-- <label>Direccón: </label> -->
+                        <input />   
+                    </div>
+                    <div class="filter-button">
+                        <button class="button"><SearchIcon class="w-5 text-gray-700" /></button>
+                    </div>    
+
+                </div>     
+
+                <div class="card-body">
+                    <div class="bg-white overflow-hidden sm:rounded-lg border border-gray-200">
+                        <table class="table w-full whitespace-nowrap">
+                            <tr class="table-header">
+                                <th>ID</th> 
+                                <th>Fecha</th> 
+                                <th>Dirección</th> 
+                                <th>Cliente</th> 
+                                <th>Empresa</th> 
+                                <th>Chofer</th> 
+                                <th></th> 
+                            </tr>
+                            <tbody>
+                                <tr class="table-row">
+                                    <td>10</td>
+                                    <td>03/09/2022</td>
+                                    <td>Av. Maipú 2034, B1636AAN Olivos</td>
+                                    <td>John Doe</td>
+                                    <td>Contratista Pilar</td>
+                                    <td>Martin Lugo</td>
+                                    <td><button><ChevronDownIcon class="w-4 text-gray-700" /></button></td>
+                                </tr>
+                                <tr class="table-row">
+                                    <td>11</td>
+                                    <td>03/09/2022</td>
+                                    <td>Pedro Goyena 2820, B1636BXR Olivos</td>
+                                    <td>John Doe</td>
+                                    <td>Contratista Pilar</td>
+                                    <td>Martin Lugo</td>
+                                    <td><button><ChevronDownIcon class="w-4 text-gray-700" /></button></td>
+                                </tr>
+                                <tr class="table-row">
+                                    <td>12</td>
+                                    <td>03/09/2022</td>
+                                    <td>Diego Palma 2847, B1642FLE San Isidro</td>
+                                    <td>John Doe</td>
+                                    <td>Contratista Pilar</td>
+                                    <td>Martin Lugo</td>
+                                    <td><button><ChevronDownIcon class="w-4 text-gray-700" /></button></td>
+                                </tr>
+                            </tbody>  
+    
+                        </table>                    
+                    </div> 
+                </div>
+            </div>
+        </div>
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <schedule :view="showFilter" />
@@ -23,11 +111,14 @@
 import { defineComponent } from 'vue'
 import AppLayout from '@/Layouts/AppLayout.vue';
 import Schedule from './Schedule.vue'
+import { ChevronDownIcon, SearchIcon } from '@heroicons/vue/solid'
 
 export default defineComponent({
     components: {
         AppLayout,
         Schedule,
+        ChevronDownIcon, 
+        SearchIcon
     },
 
     data() {
